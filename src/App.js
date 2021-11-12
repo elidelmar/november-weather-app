c6921361020e39c8a3953a0c483951ee3033e959
elidelmar <88523575+elidelmar@users.noreply.github.com>
2021-11-11 20:15:48 -0500

    updated js

--- src/App.js
+++ src/App.js
@@ -8,26 +8,6 @@ export default function App() {
     <div className="App">
       <div className="container">
         <Weather defaultCity="Charlotte" />
-
-        <footer>
-          Open-Sourced Code on GitHub 
-          {" "}
-      
-          <a
-            href=""
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-          </a>{" "}
-          and{" "}
-          <a
-            href="https://suspicious-beaver-111c4d.netlify.com/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            hosted on Netlify
-          </a>
-        </footer>
       </div>
     </div>
   );
